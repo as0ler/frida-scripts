3bf0478970dfe6b61f61a20c4f8332eed06f5d16
as0ler <dark.cancerbero@gmail.com>
2016-03-28 00:48:25 +0200

    Add SecItemUpdate and SecItemDelete on keychainItem_Hooker

--- keychainItem_Hooker.py
+++ keychainItem_Hooker.py
@@ -17,58 +17,61 @@ def do_hook():
 
     hook = """
 	var className = "Security";
-	var methodName = "SecItemAdd";
+	var hookMethods = ["SecItemAdd", "SecItemUpdate", "SecItemDelete"];
 
-	send("Hooking class : " + className);
-    
-	var ptr = null;
-	Module.enumerateExports(className, { 
-		onMatch: function(imp) {
-			if (imp.type == "function" && imp.name == "SecItemAdd") {
-				send("Found target method : " + methodName);
-			
-				try {
-					Interceptor.attach(ptr(imp.address), {
-						onEnter: function(args) { 
-							send("Hooking method : " + methodName);
-							var params = ObjC.Object(args[0]); // CFDictionaryRef => NSDictionary
-							var keys = params.allKeys();
-							for (index = 0; index < keys.count(); index++) {
-								var k = keys.objectAtIndex_(index);
-								var v = params.objectForKey_(k);
-								if (k == "v_Data") {
-									var string = ObjC.classes.NSString.alloc();
-									v = string.initWithData_encoding_(v,4).toString();
-								}
-								if (k == "pdmn") {
-									if (v == "ak") { 
-										v = "kSecAttrAccessibleWhenUnlocked";
-									} else if (v == "ck") {
-										v = "kSecAttrAccessibleAfterFirstUnlock";
-									} else if (v == "dk") {
-										v = "kSecAttrAccessibleAlways";
-									} else if (v == "aku") {
-										v = "kSecAttrAccessibleWhenUnlockedThisDeviceOnly"
-									} else if (v == "cku") {
-										v = "kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly";
-									} else {
-										// v == dku
-										v = "kSecAttrAccessibleAlwaysThisDeviceOnly";
+	for (index = 0; index < hookMethods.length; index++) {
+		var methodName = hookMethods[index];
+		send("Hooking class : " + className);
+	    
+		var ptr = null;
+		Module.enumerateExports(className, { 
+			onMatch: function(imp) {
+				if (imp.type == "function" && imp.name == methodName) {
+					send("Found target method : " + methodName);
+				
+					try {
+						Interceptor.attach(ptr(imp.address), {
+							onEnter: function(args) { 
+								send("Hooking method : " + imp.name);
+								var params = ObjC.Object(args[0]); // CFDictionaryRef => NSDictionary
+								var keys = params.allKeys();
+								for (index = 0; index < keys.count(); index++) {
+									var k = keys.objectAtIndex_(index);
+									var v = params.objectForKey_(k);
+									if (k == "v_Data") {
+										var string = ObjC.classes.NSString.alloc();
+										v = string.initWithData_encoding_(v,4).toString();
+									}
+									if (k == "pdmn") {
+										if (v == "ak") { 
+											v = "kSecAttrAccessibleWhenUnlocked";
+										} else if (v == "ck") {
+											v = "kSecAttrAccessibleAfterFirstUnlock";
+										} else if (v == "dk") {
+											v = "kSecAttrAccessibleAlways";
+										} else if (v == "aku") {
+											v = "kSecAttrAccessibleWhenUnlockedThisDeviceOnly"
+										} else if (v == "cku") {
+											v = "kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly";
+										} else {
+											// v == dku
+											v = "kSecAttrAccessibleAlwaysThisDeviceOnly";
+										} 
 									} 
-								} 
-							    	send("   " + k + "=" + v);
+									send("   " + k + "=" + v);
+								}
 							}
-						}
-					});
-				} catch (error) {
-					console.log("Ignoring " + imp.name + ": " + error.message);
+						});
+					} catch (error) {
+						console.log("Ignoring " + imp.name + ": " + error.message);
+					}
 				}
+			},
+			onComplete: function (e) {
+					send("All methods loaded");
 			}
-		},
-		onComplete: function (e) {
-				send("All methods loaded");
-		}
-	});
+		});
+	}
 	"""
     return hook
 
