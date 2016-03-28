83321b3ad793d384d22c6f602edc9511663d7b82
as0ler <dark.cancerbero@gmail.com>
2016-03-28 22:18:40 +0200

    Add Spawn Option

--- hooker.py
+++ hooker.py
@@ -1,6 +1,7 @@
 import frida
 import sys
 import os 
+from optparse import OptionParser
 
 def on_message(message, data):
     try:
@@ -14,7 +15,23 @@ def on_message(message, data):
 if __name__ == '__main__':
     try:
 	path = "hooks"
-        session = frida.get_usb_device().attach(str(sys.argv[1]))
+	parser = OptionParser(usage="usage: %prog [options] <process_to_hook>",version="%prog 1.0")
+	parser.add_option("-A", "--attach", action="store_true", default=False,help="Attach to a running process")
+	parser.add_option("-S", "--spawn", action="store_true", default=False,help="Spawn a new process and attach")
+
+	(options, args) = parser.parse_args()
+	if (options.spawn):
+		print ("[*] Spawning "+ str(args[0]))
+       		pid = frida.get_usb_device().spawn([args[0]])
+		session = frida.get_usb_device().attach(pid) 
+	elif (options.attach):
+		print ("[*] Attaching to "+str(args[0]))
+       		session = frida.get_usb_device().attach(str(args[0]))
+	else:
+		print ("Error")
+		print ("[X] Option not selected. View --help option.")
+		sys.exit(0)
+
 	for filename in os.listdir(path):
 		name, ext = os.path.splitext(filename)
 		if (ext == ".enabled"):
